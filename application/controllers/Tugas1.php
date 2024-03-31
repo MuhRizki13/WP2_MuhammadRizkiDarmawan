528168461593818a76bf7847675a312c2d34328f
MuhRizki13 <m.rizki.darmawan2020@gmail.com>
2024-03-31 16:02:53 +0700

    Tugas1

--- application/controllers/Tugas1.php
+++ application/controllers/Tugas1.php
@@ -1,10 +1,16 @@
 <?php
-class Tugas1 extends CI_Controller
+class Tugas1 extends CI_Controller 
 {
     public function index()
 {
-    echo "<h1>Perkenalkan</h1>";
-    echo"Nama Saya Muhammad Rizki Darmawan Saya tinggal di daerah Kabupaten Mempawah olah raga yang saya sukai Futsal dan Sepak Bola";
+    echo "<h1><center>introduce</center></h1>";
+    echo "<h5><div style='text-align: center; padding: 10px;'> <p style='text-align: justify;'>NAMA SAYA MUHAMMAD RIZKI DARMAWAN
+         SAYA TINGGAL DI KABUPATEN MEMPAWAH TEPATNYA DI DESA PASIR KECAMATAN MEMPAWAH HILIR
+         HOBY SAYA BERMAIN BOLA DAN FUTSAL</p>
+         </div></h5>";
+    echo "<h5><center>SEKIAN BIODATA SINGKAT DARI SAYA</center></h5>";
+    echo "<h5><center>KEPASAR MENTUTU MEMBELI BERANG BERANG</center></h5>";
+    echo "<h5><center>I LOVE U ISTRI ORANG</center></h5>";
 }
 }
     

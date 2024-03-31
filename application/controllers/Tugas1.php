38fef963bbbe924391d0d3f0907991bb6cb91b67
MuhRizki13 <m.rizki.darmawan2020@gmail.com>
2024-03-31 16:17:06 +0700

    Tugas1

--- application/controllers/Tugas1.php
+++ application/controllers/Tugas1.php
@@ -3,7 +3,7 @@ class Tugas1 extends CI_Controller
 {
     public function index()
 {
-    echo "<h1><center>introduce</center></h1>";
+    echo "<h1><center>INTRODUCE</center></h1>";
     echo "<h5><div style='text-align: center; padding: 10px;'> <p style='text-align: justify;'>NAMA SAYA MUHAMMAD RIZKI DARMAWAN
          SAYA TINGGAL DI KABUPATEN MEMPAWAH TEPATNYA DI DESA PASIR KECAMATAN MEMPAWAH HILIR
          HOBY SAYA BERMAIN BOLA DAN FUTSAL</p>
